92a10de6d066d056701fbbb1dda13b7541fb1e78
Suyooo (Server) <me@suyo.be>
2023-05-18 10:51:46 +0000

    patch list pagination

--- views/patchlist.php
+++ views/patchlist.php
@@ -3,9 +3,11 @@ $params = array_slice(explode('/', getenv('REQUEST_URI')), 3);
 require_once('../includes/general.php');
 require_once('../includes/units.php');
 
+$page = count($params) > 0 ? max(((int)$params[0])-1, 0) : 0;
+
 $patchlist = array();
-$query = get_db_connection()->prepare('SELECT patch_id, patch_name FROM patches ORDER BY patch_id DESC');
-$query->execute();
+$query = get_db_connection()->prepare('SELECT patch_id, patch_name FROM patches ORDER BY patch_id DESC LIMIT :page, 20');
+$query->execute([':page' => $page * 20]);
 while ($patch_row = $query->fetch(\PDO::FETCH_ASSOC)) {
 	if ($patch_row["patch_name"] == "PENDING") continue;
 	$patch = ["id" => $patch_row["patch_id"], "name" => $patch_row["patch_name"], "list" => array()];
@@ -19,5 +21,5 @@ while ($patch_row = $query->fetch(\PDO::FETCH_ASSOC)) {
 	$patchlist[] = $patch;
 }
 
-echo $templates->render('patch_list', ["patches" => $patchlist]);
+echo $templates->render('patch_list', ["patches" => $patchlist, "page" => $page]);
 ?>
